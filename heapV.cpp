0c9d22af1681382731616121f6042bd043867bbb
Jack Fenton <mllax8@me.com>
2020-06-12 14:37:43 -0700

    Adding Their Things

---
+++ heapV.cpp
@@ -0,0 +1,8 @@
+//
+// Project by Jack Fenton
+// Northeastern University Department of Computer and Electrical Engineering
+// EECE2560 Introduction to Engineering Algorithms
+// Project begun on 2020-06-12.
+//
+
+#include "heapV.h"
--- main.cpp
+++ main.cpp
@@ -1,7 +1,75 @@
 #include <iostream>
+#include <limits.h>
+#include <vector>
+#include <list>
+#include <fstream>
+#include <queue>
+
+#include <boost/graph/adjacency_list.hpp>
+
+using namespace boost;
+using namespace std;
+
+struct VertexProperties;
+struct EdgeProperties;
+
+typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
+
+struct VertexProperties
+{
+	pair<int,int> cell; // maze cell (x,y) value
+	Graph::vertex_descriptor pred; // predecessor node
+	int weight;
+	bool visited;
+	bool marked;
+};
+
+// Create a struct to hold properties for each edge
+struct EdgeProperties
+{
+	int weight;
+	bool visited;
+	bool marked;
+};
+
+typedef adjacency_list<vecS, vecS, bidirectionalS, VertexProperties, EdgeProperties> Graph;
+
+// typedef property<edge_weight_t, int> EdgeProperty;
+
+#define LargeValue 99999999
+
+void clearVisited(Graph &g)
+// Mark all nodes in g as not visited.
+
+void setNodeWeights(Graph &g, int w)
+// Set all node weights to w.
+
+void clearMarked(Graph &g)
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	try
+	{
+		ifstream fin;
+		
+		// Read the maze from the file.
+		string fileName = "yourpath/maze1.txt";
+		
+		fin.open(fileName.c_str());
+		if (!fin)
+		{
+			cerr << "Cannot open " << fileName << endl;
+			exit(1);
+		}
+		
+		maze m(fin);
+		fin.close();
+		
+		m.print(m.numRows()-1,m.numCols()-1,0,0);
+		
+		Graph g;
+		m.mapMazeToGraph(g);
+		
+		cout << g << endl;
+	}
 }
